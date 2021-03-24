0ba606da804f25407b3d592fc9630da9f415c86a
Vlad Goncharov <ganchmix@gmail.com>
2021-03-24 18:14:28 +0200

    Form

---
+++ form.js
@@ -0,0 +1,159 @@
+class Input {
+    constructor(selector) {
+        this.$el = selector;
+    }
+
+    getVal() {
+        return this.$el.val();
+    }
+
+    clearVal() {
+        this.$el.val('');
+    }
+
+    showError() {
+        this.$el.after('<div class="error">' + this.error + '</div>');
+    }
+
+    removeError() {
+        if (this.$el.next().hasClass('error')) {
+            this.$el.next().remove();
+
+        }
+    }
+
+}
+
+class FullName extends Input {
+    validate() {
+        if (!this.$el.val()) {
+            this.error = 'Please enter your name';
+            return false;
+        }
+
+        return true;
+    }
+}
+
+class Email extends Input {
+    validate() {
+        if (!this.$el.val()) {
+            this.error = 'Please enter your email';
+            return false;
+        }
+
+        let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!regex.test(this.$el.val())) {
+            this.error = 'Email is not valid';
+            return false;
+        }
+
+        return true;
+    }
+}
+
+class Phone extends Input {
+    validate() {
+        let regex = /^[0-9]+$/
+
+        if (this.$el.val() && !regex.test(this.$el.val())) {
+            this.error = 'Only numerical is allowed';
+            return false;
+
+        }
+
+        return true;
+    }
+}
+
+class Text extends Input {
+    validate() {
+        if (this.$el.val().length < 10 || !this.$el.val()) {
+            this.error = '10 symbols is minimum';
+            return false;
+        }
+
+        return true;
+    }
+}
+
+$(document).ready(function () {
+    let form = $('.form');
+
+    let submit = form.find('button');
+
+    let email = new Email(form.find('input[name=email]'));
+    let fullName = new FullName(form.find('input[name=name]'));
+    let phone = new Phone(form.find('input[name=phone]'));
+    let text = new Text(form.find('textarea'));
+
+    submit.on('click', function () {
+        let valid = true;
+
+        fullName.removeError();
+        if (!fullName.validate()) {
+            fullName.showError();
+            valid = false;
+
+        }
+
+        email.removeError();
+        if (!email.validate()) {
+            email.showError();
+            valid = false;
+
+        }
+
+        phone.removeError();
+        if (!phone.validate()) {
+            phone.showError();
+            valid = false;
+
+        }
+
+        text.removeError();
+        if (!text.validate()) {
+            text.showError();
+            valid = false;
+
+        }
+
+        if (valid) {
+            if ($(this).next().hasClass('error')) {
+                $(this).next().remove();
+
+            }
+
+            $.ajax({
+                url: "https://api.floppyapp.com/contact/send",
+                data: {
+                    full_name: fullName.getVal(),
+                    email: email.getVal(),
+                    phone: phone.getVal(),
+                    text: text.getVal(),
+                },
+                method: 'POST'
+            }).done(function () {
+                $('.form-success-message').addClass('form-success-message-active');
+                $('body').css('overflow', 'hidden');
+
+                $('.form-success-message .button').on('click', function () {
+                    $('.form-success-message').removeClass('form-success-message-active');
+                    $('body').css('overflow', 'visible');
+
+                });
+
+                fullName.clearVal();
+                email.clearVal();
+                phone.clearVal();
+                text.clearVal();
+            });
+
+        } else {
+            if (!$(this).next().hasClass('error')) {
+                $(this).after('<div class="error">Please check the form</div>');
+            }
+        }
+    })
+
+})
---
+++ index.js
@@ -0,0 +1,74 @@
+// function play() {
+//     let audio = $("#audio");
+//     let audioElement = document.getElementById("audio");
+//
+//     if (audio.hasClass('player-icon-stop') === false) {
+//         audioElement.pause();
+//
+//         let icon = $('.player-icon-start');
+//
+//         icon.addClass('player-icon-stop');
+//         icon.removeClass('player-icon-start');
+//
+//         audioElement.pause();
+//
+//     } else {
+//         let icon = $('.player-icon-stop');
+//         console.log(audio.hasClass('player-icon-stop'));
+//
+//         icon.addClass('player-icon-start');
+//         icon.removeClass('player-icon-stop');
+//
+//         audioElement.play();
+//
+//     }
+//
+// }
+
+function replaceContent(content) {
+    let contents = $('.content-box .content');
+
+    showNecessaryText(contents);
+
+    function showNecessaryText(elements) {
+        elements.each(function () {
+            if ($(this).data('content') !== content) {
+                $(this).css('display', 'none');
+
+            } else {
+
+                $('.bg').css('filter', 'blur(100px)');
+
+                setTimeout(function () {
+                    $('.bg').css('filter', 'blur(3px)');
+
+                }, 150);
+
+                $(this).fadeIn('slow');
+            }
+
+        });
+
+    }
+
+}
+
+$('.header > ul > li > a').on('click', function () {
+    $('.active').removeClass('active');
+
+    replaceContent($(this).data('content'));
+
+    $(this).addClass('active');
+
+});
+
+replaceContent('home');
+
+$('.bg').css('width', '58%');
+
+setInterval(function () {
+    let bgVideo = $('.bg video');
+    bgVideo.css({'filter': 'blur(0px)'});
+    setTimeout(() => bgVideo.css({'filter': 'opacity(0.01)'}), 700);
+
+}, 5000);
